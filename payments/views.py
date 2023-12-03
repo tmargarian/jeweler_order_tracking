105a9276b75c1b445e46db81c1937d806505a84c
Tigran Margarian <tig.margarian@gmail.com>
2023-12-02 17:21:48 -0800

    metadata updates

--- payments/views.py
+++ payments/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
-from djstripe.models import Product
+from djstripe.models import Product, Plan
 from django.conf import settings
+from payments.metadata import product_metadata_dict
 
 
 class PricingPageView(TemplateView):
@@ -9,6 +10,16 @@ class PricingPageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["stripe_pricing_table_id"] = settings.STRIPE_PRICING_TABLE_ID
-        context["stripe_public_key"] = settings.STRIPE_TEST_PUBLIC_KEY  # Replace with Live key in prod
-        context["products"] = Product.objects.all()
+        # Replace with Live key in prod
+        context["stripe_public_key"] = settings.STRIPE_TEST_PUBLIC_KEY
+
+        # Products context enriched with info from metadata.py
+        products = Product.objects.filter(active=True)  # Only active (non-deleted) products
+        for product in products:
+            product.metadata = product_metadata_dict.get(product.id, None)
+        context["products"] = products
+
+        # Plan information
+        context["plans"] = Plan.objects.all()
+
         return context
