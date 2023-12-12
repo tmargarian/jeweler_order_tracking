4df8f1b4bf6158bb21ca3575385c6d0f45f48b8f
RobertMargarian <margarian.robert@yahoo.com>
2023-12-12 09:33:07 -0800

    Order Functionality Update
    1) New models added in order_tracking and accounts models.py
    2) New urls for orders and notes
    3) New views and forms in order_tracking folder
    4) New templates in order_tracking templates

--- order_tracking/urls.py
+++ order_tracking/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import (
+    OrderListView, OrderCreateView, OrderUpdateView, OrderDeleteView,
+    NoteUpdateView, NoteDeleteView,
+)
+
+app_name = "order_tracking"
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home")
+    path("order_list/", OrderListView.as_view(), name="order_list"),
+    path("order_create/", OrderCreateView.as_view(), name="order_create"),
+    path("order_update/<uuid:pk>/", OrderUpdateView.as_view(), name="order_update"),
+    path("order_delete/<uuid:pk>/", OrderDeleteView.as_view(), name="order_delete"),
+
+    path("note_update/<pk>/", NoteUpdateView.as_view(), name="note_update"),
+    path("note_delete/<pk>/", NoteDeleteView.as_view(), name="note_delete"),
 ]
