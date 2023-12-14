c201a134668c8b8100cbf9712754cc29b50a2f8a
Tigran Margarian <tig.margarian@gmail.com>
2023-12-13 21:06:34 -0800

    Separating non-essential user info into profile relation

--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,21 @@ from django.conf import settings
 from django.db import models
 
 
+# Crucial data will be stored in CustomUser; this is what's necessary to finish Signup
 class CustomUser(AbstractUser):
     # Email is the only field that CANNOT be empty to complete signup
     email = models.EmailField(max_length=100, blank=False, null=False)
     # The rest of the fields can be filled out after signup
+    company = models.ForeignKey("Company", on_delete=models.CASCADE, related_name="users", blank=True, null=True)
     is_owner = models.BooleanField(default=True)
     is_employee = models.BooleanField(default=False)
-    company = models.ForeignKey("Company", on_delete=models.CASCADE, related_name="users", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+# Secondary data stored in the UserProfile (phone numbers | full name)
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="profile")
     first_name = models.CharField(max_length=100, blank=True, null=True)
     last_name = models.CharField(max_length=100, blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
